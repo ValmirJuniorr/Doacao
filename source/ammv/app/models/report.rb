1d0f4f2b0c4387aef54c106f22c1f46e73a7aca6
ValmirJuniorr <valmir_junyor@hotmail.com>
2017-05-09 15:29:11 -0300

    generation of file

--- source/ammv/app/models/report.rb
+++ source/ammv/app/models/report.rb
@@ -3,10 +3,20 @@ class Report < ApplicationRecord
 	attr_accessor :initial_date, :end_date
 
 	def generate
-		result ="A"
-		@cadastros = Cadastro.where(data_ocorrencia: (@initial_date)..@end_date)
+		result ="A201ASSOC MARIA MAE VIDA"
+
+		@cadastros = Cadastro.all #where(data_ocorrencia: (@initial_date)..@end_date)
 		@cadastros.each do |cadastro|
-		  puts cadastro
+			line = "D#{cadastro.id_cliente_coelce}"
+			result += "\n#{line}"
 		end
-	end	
+		puts result
+	end
+
+	def testing
+		fileName="app/report/CEX.AMMDV.#{Time.now.strftime("%Y%m%d")}.SOL"		
+		File.open(fileName, 'w') do |f2|
+			f2.puts "Só sucesso";
+		end
+    end
 end
