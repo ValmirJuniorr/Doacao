d0cc07e5a36a5c8f190c7d118bb91f431d7bd687
ValmirJuniorr <valmir_junyor@hotmail.com>
2017-05-10 17:46:07 -0300

    Method to generate Report Ready

--- source/ammv/app/models/report.rb
+++ source/ammv/app/models/report.rb
@@ -2,21 +2,36 @@ class Report < ApplicationRecord
 
 	attr_accessor :initial_date, :end_date
 
-	def generate
-		result ="A201ASSOC MARIA MAE VIDA"
 
-		@cadastros = Cadastro.all #where(data_ocorrencia: (@initial_date)..@end_date)
-		@cadastros.each do |cadastro|
-			line = "D#{cadastro.id_cliente_coelce}"
-			result += "\n#{line}"
-		end
-		puts result
+	#Function to Formater number to n decimal with zeros
+	def fnd(n, number)
+		"%0#{n}d" % number
 	end
+	
+	def generate (sequence_number)
+		generateDate =Time.now.strftime("%Y%m%d")
+		fileName      = "app/report/CEX.AMMDV.#{generateDate}.SOL"		
+		registroA     = "A201ASSOC MARIA MAE VIDA#{generateDate}#{fnd(6,sequence_number)}"
+		registroD     = ""
+		
+		totalRegister = 2 		
+		valueTotal    = 0
+		puts "de #{@initial_date} to #{@end_date}"
+		@cadastros = Cadastro.where(data_ocorrencia: (@initial_date)..@end_date)
+			@cadastros.each do |c|
+			line = 'D'+fnd(10, c.id_cliente_coelce)+c.digito_verificador_cliente_coelce+fnd(2,c.codigo_ocorrencia)+
+					c.data_ocorrencia.strftime('%m/%d/%Y')+fnd(9,(c.valor* 100))+ fnd(2, c.parcelas)+
+					fnd(8,c.id_cliente_parceira)+fnd(4, c.codigo_produto) + c.codigo_empresa_parceira
 
-	def testing
-		fileName="app/report/CEX.AMMDV.#{Time.now.strftime("%Y%m%d")}.SOL"		
-		File.open(fileName, 'w') do |f2|
-			f2.puts "Só sucesso";
+			registroD += "\n#{line}"
+
+			totalRegister +=1
+			valueTotal += c.valor
 		end
-    end
+		registroZ = "\nZ"+fnd(6,totalRegister)+fnd(9,(valueTotal*100))
+
+		File.open(fileName, 'w') do |reportFile|
+			reportFile.puts registroA+registroD+registroZ			
+		end
+	end
 end
