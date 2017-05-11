ab2b4e11653d6992344f1187045b27dbafdfea90
Davi Guimaraes <davi.guimaraesleite@gmail.com>
2017-05-11 15:38:20 -0300

    Implementacao de Front-End

--- source/ammv/app/assets/javascripts/application.js
+++ source/ammv/app/assets/javascripts/application.js
@@ -12,6 +12,27 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap-sprockets
 //= require turbolinks
+//= require bootstrap-sprockets
 //= require_tree .
+
+$(function () {
+    $( '#table' ).searchable({
+        striped: true,
+        oddRow: { 'background-color': '#f5f5f5' },
+        evenRow: { 'background-color': '#fff' },
+        searchType: 'fuzzy'
+    });
+    
+    $( '#searchable-container' ).searchable({
+        searchField: '#container-search',
+        selector: '.row',
+        childSelector: '.col-xs-4',
+        show: function( elem ) {
+            elem.slideDown(100);
+        },
+        hide: function( elem ) {
+            elem.slideUp( 100 );
+        }
+    })
+});
